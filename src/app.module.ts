bd638ace23fe2faf271abb8450c726c602694e9f
01tranduc <tranngocminhduc2411@gmail.com>
2023-10-06 11:27:22 +0700

    create db create usermodule productmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: '01tranduc',
+      password: '1234567Duc',
+      database: 'DB',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule, 
+    ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
