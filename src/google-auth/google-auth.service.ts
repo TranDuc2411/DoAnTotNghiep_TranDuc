6203ffd1515777524d3c47fb367c2798bb4991ba
01tranduc <tranngocminhduc2411@gmail.com>
2023-10-09 02:04:26 +0700

    end module google-auth

--- src/google-auth/google-auth.service.ts
+++ src/google-auth/google-auth.service.ts
@@ -6,7 +6,7 @@ dotenv.config();
 @Injectable()
 export class GoogleAuthService {
   async generateToken(user: any): Promise<string> {
-    const payload = { sub: user.displayName, email: user.email, role: 0 };
+    const payload = { username: user.displayName, email: user.email, role: 0 };
     // const payload = { sub: user.displayName, email: user.email };
     const token = jwt.sign(payload, process.env.TOKEN_KEY, {
       expiresIn: '1h',
