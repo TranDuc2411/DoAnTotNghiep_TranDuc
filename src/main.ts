c0455709e00ef871808076247afdf6bc6236ae82
01tranduc <tranngocminhduc2411@gmail.com>
2023-10-08 14:34:10 +0700

    okok

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
+  // API doc
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.DB_USERNAME);
+
+  const config = new DocumentBuilder()
+    .setTitle('Do An Tot Nghiep - Tran Ngoc Minh Duc')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('User')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // config môi trường
+  dotenv.config();
 
   await app.listen(process.env.SERVER_PORT);
 }
--- src/user/user.DTO.ts
+++ src/user/user.DTO.ts
@@ -1,18 +1,22 @@
 // user.dto.ts
 
 import { IsString, IsNumber, IsOptional } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class UserDto {
+  @ApiProperty()
   @IsString()
   username: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 
+  // @ApiProperty()
   @IsNumber()
   @IsOptional()
   role?: number; // Dấu '?' cho phép role là một trường tùy chọn
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   email?: string; // Dấu '?' cho phép email là một trường tùy chọn
